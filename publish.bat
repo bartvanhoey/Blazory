08bbb5c5c0fc483d7d7fc2114cccc513b909545b
bartvanhoey <bartvanhoey@hotmail.com>
2020-07-31 03:20:25 +0200

    update

--- publish.bat
+++ publish.bat
@@ -1,10 +1,3 @@
-REM git add .
-REM git commit -m "update"
-
-REM npm version patch
-
-REM git push
-
 git add .
 git commit -m "update"
 git push
@@ -12,3 +5,5 @@ git push
 npm version patch
 git push —-tags origin master
 npm publish
+
+git push
