11af5dfcac2af96645984496eef7cd1adbc5fa2b
bartvanhoey <bartvanhoey@hotmail.com>
2020-08-04 13:08:26 +0200

    v0.0.95

--- Publish.bat
+++ Publish.bat
@@ -2,7 +2,7 @@ node -p "require('./package.json').version"
 
 set /p CURRENT_VERSION="Enter new version number:"
 
-echo version= %CURRENT_VERSION%
+echo new version=%CURRENT_VERSION%
 
 git add .
 git commit -m %CURRENT_VERSION%
